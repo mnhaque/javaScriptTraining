d00df3e921f7352a868a9c5a96514da12554a195
nasim <mahammed_haque@epam.com>
2017-08-07 17:43:46 +0530

    changes to closuresToShareClassState.js

--- javaScriptTraining/homeWork4/closuresToShareClassState.js
+++ javaScriptTraining/homeWork4/closuresToShareClassState.js
@@ -1,29 +1,34 @@
-// all unit test cases are passing except 
-// 1. should not allow cats to be instantiated without a name and an age
-//      Expected an error to be thrown
-//2.  Uses Object.defineProperty to get and set instance weight
-//      Value is not what was expected
 var Cat = (function () {
     var array = [];
 
     function Cat(name, weight) {
+        var weight_temp;
         if (!name || !weight) {
-            new Error("height or weight can not be empty");
+            throw new Error("height or weight can not be empty");
         } else {
             this.name = name;
-            this.weight = weight;
-            array.push(this);
+            weight_temp = weight;
+            array.push(weight_temp)
         }
+        Object.defineProperty(this, 'weight', {
+            set: function (weight) {
+                array.splice(array.indexOf(weight_temp), 1);
+                weight_temp = weight;
+                array.push(weight);
+            },
+            get: function () {
+                return weight_temp;
+            }
+        });
     }
     Object.defineProperty(Cat, "averageWeight", {
         value: function () {
             var sum = 0;
             for (var i = 0; i < array.length; i++) {
-                sum += array[i].weight;
+                sum += array[i];
             }
             return sum / array.length;
         }
     });
-
     return Cat;
 }());
